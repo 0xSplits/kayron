e8defed744640757ed635db231e6d9a57c399fd4
xh3b4sd <xh3b4sd@gmail.com>
2025-07-17 13:02:52 +0200

    fix

--- pkg/schema/specification/service/service.go
+++ pkg/schema/specification/service/service.go
@@ -14,7 +14,7 @@ type Service struct {
 }
 
 func (s Service) Empty() bool {
-	return s.Deploy.Empty() && s.Github.Empty() && s.Deploy.Empty()
+	return s.Docker.Empty() && s.Github.Empty() && s.Deploy.Empty()
 }
 
 func (s Service) Verify() error {
