1bd3e5eda16806e1cb744bf80636730fb39faa14
xh3b4sd <xh3b4sd@gmail.com>
2025-09-24 13:04:09 +0200

    fix

--- pkg/operator/container/task.go
+++ pkg/operator/container/task.go
@@ -3,6 +3,7 @@ package container
 import (
 	"context"
 	"slices"
+	"sort"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
@@ -60,17 +61,72 @@ func (c *Container) task(det []detail) ([]task, error) {
 		}
 
 		for _, x := range out.Services {
-			if aws.ToString(x.Status) != "ACTIVE" {
-				// There might be inactive or draining services with our desired service
-				// labels in case we updated CloudFormation stacks multiple times during
-				// with preview deployments during testing. We only want to consider the
-				// current state of those stacks that are still active, because the
-				// inactive versions have most likely been deleted already.
+			// For our reconciliation of the current state here to make sense we have
+			// to ensure that the services being managed are actually controlled by
+			// ECS. If we do not check this we may end up getting the wrong kind of
+			// deployment information that we rely on to be provided in a certain way
+			// below.
+
+			if x.DeploymentController.Type != types.DeploymentControllerTypeEcs {
+				c.log.Log(
+					"level", "warning",
+					"message", "skipping reconciliation for ECS service",
+					"reason", "service does not use ECS controller",
+					"cluster", *x.ClusterArn,
+					"service", *x.ServiceArn,
+				)
 
 				{
 					continue
 				}
 			}
+
+			// There might be inactive or draining services with our desired service
+			// labels in case we updated CloudFormation stacks multiple times during
+			// with preview deployments during testing. We only want to consider the
+			// current state of those stacks that are still active, because the
+			// inactive versions have most likely been deleted already.
+
+			if aws.ToString(x.Status) != "ACTIVE" {
+				c.log.Log(
+					"level", "debug",
+					"message", "skipping reconciliation for ECS service",
+					"reason", "ECS service is inactive",
+					"cluster", *x.ClusterArn,
+					"service", *x.ServiceArn,
+				)
+
+				{
+					continue
+				}
+			}
+
+			// Try to find the task definition that is successfully deployed. Here we
+			// prevent picking those new task definitions prematurely that are still
+			// transitioning during deployments.
+
+			var arn string
+			{
+				arn = tasArn(x.Deployments)
+			}
+
+			if arn == "" {
+				c.log.Log(
+					"level", "debug",
+					"message", "skipping reconciliation for ECS service",
+					"reason", "ECS service has no completed task definition",
+					"cluster", *x.ClusterArn,
+					"service", *x.ServiceArn,
+				)
+
+				{
+					continue
+				}
+			}
+
+			// Try to identify the service and task definition based on their resource
+			// tags.  If a service is not labelled with our desired 'service' tag,
+			// then we cannot work with it properly moving forward.
 
 			var pre string
 			var ser string
@@ -97,7 +153,7 @@ func (c *Container) task(det []detail) ([]task, error) {
 			// executes for its own index.
 
 			tas[i] = task{
-				arn: *x.TaskDefinition,
+				arn: arn,
 				pre: pre,
 				ser: ser,
 			}
@@ -143,6 +199,25 @@ func serTag(tag []types.Tag, key string) string {
 	for _, x := range tag {
 		if aws.ToString(x.Key) == key {
 			return aws.ToString(x.Value)
+		}
+	}
+
+	return ""
+}
+
+func tasArn(dep []types.Deployment) string {
+	// Make sure we can select the newest deployment first, so sort by time in
+	// descending direction.
+
+	sort.Slice(dep, func(i, j int) bool {
+		return aws.ToTime(dep[i].CreatedAt).After(aws.ToTime(dep[j].CreatedAt))
+	})
+
+	// Now pick the task definition ARN of the first completed deployment.
+
+	for _, y := range dep {
+		if y.RolloutState == types.DeploymentRolloutStateCompleted {
+			return aws.ToString(y.TaskDefinition)
 		}
 	}
 
--- pkg/operator/status/preview.go
+++ pkg/operator/status/preview.go
@@ -12,6 +12,9 @@ import (
 )
 
 const (
+	// marker identifies those issue comments managed by the operator. This marker
+	// is effectively a markdown comment that helps us find and update our status
+	// updates.
 	marker = "<!-- kayron:preview:status -->"
 )
 
@@ -30,6 +33,8 @@ func (s *Status) preview() error {
 	fnc := func(_ int, o cache.Object) error {
 		var err error
 
+		// Try to find our marked issue comment for any given preview deployment.
+
 		var com *github.IssueComment
 		{
 			com, err = s.issCom(o)
@@ -38,11 +43,16 @@ func (s *Status) preview() error {
 			}
 		}
 
+		// Determine the status field of our status update. This also tells us
+		// whether we should update the issue comment if it exists.
+
 		var sta string
 		var upd bool
 		{
 			sta, upd = s.comSta(o, com)
 		}
+
+		// Create or update the issue comment with any given status, if any.
 
 		if com == nil {
 			err = s.creCom(sta, o)
@@ -98,9 +108,9 @@ func (s *Status) creCom(sta string, obj cache.Object) error {
 // update for preview deployments.
 //
 //	<!-- kayron:preview:status -->
-//	Status | Hash | Endpoint
+//	Status | Commit | Endpoint
 //	---|----|---
-//	Creating | 1D0FD508 | 1d0fd508.lite.testing.splits.org
+//	Updating | c8e29b2 | 1d0fd508.lite.testing.splits.org
 func (s *Status) comBod(sta string, obj cache.Object) string {
 	var dom string
 	{
@@ -112,17 +122,17 @@ func (s *Status) comBod(sta string, obj cache.Object) string {
 		end = fmt.Sprintf("[%s](https://%s)", dom, dom)
 	}
 
-	var hsh string
+	var com string
 	{
-		hsh = fmt.Sprintf("`%s`", obj.Release.Labels.Hash.Upper())
+		com = shoStr(obj.Artifact.Scheduler.Current, 7)
 	}
 
 	return strings.Join(
 		[]string{
 			marker,
-			fmt.Sprintf("%s | %s | %s", "Status", "Hash", "Endpoint"),
+			fmt.Sprintf("%s | %s | %s", "Status", "Commit", "Endpoint"),
 			/*********/ "---|----|---",
-			fmt.Sprintf("%s | %s | %s", sta, hsh, end),
+			fmt.Sprintf("%s | %s | %s", sta, com, end),
 		},
 		"\n")
 }
@@ -203,4 +213,14 @@ func (s *Status) updCom(sta string, obj cache.Object, cid int64) error {
 	}
 
 	return nil
+}
+
+// shoStr returns teh given string with a maximum length of max, or the provided
+// string itself if that string is shorter than max.
+func shoStr(str string, max int) string {
+	if len(str) >= max {
+		return str[:max]
+	}
+
+	return str
 }
